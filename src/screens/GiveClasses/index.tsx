aaa68afd1e509fa815c79f81c5fcf628f9d65333
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 17:38:16 -0300

    feat: finish GiveClasses screen structure

--- src/screens/GiveClasses/index.tsx
+++ src/screens/GiveClasses/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import giveClassesBgImg from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 const GiveClasses: React.FC = () => (
-  <View />
+  <View style={styles.container}>
+    <ImageBackground
+      source={giveClassesBgImg}
+      resizeMode="contain"
+      style={styles.content}
+    >
+      <Text style={styles.title}>Quer ser um Proffy?</Text>
+      <Text style={styles.description}>
+        Para começar, você precisa se cadastrar como professor em nossa plataforma web.
+      </Text>
+    </ImageBackground>
+
+    <RectButton style={styles.okButton}>
+      <Text style={styles.okButtonText}>Tudo bem</Text>
+    </RectButton>
+  </View>
 );
 
 export default GiveClasses;
