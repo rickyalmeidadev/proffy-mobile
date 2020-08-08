f5fa5900fb56e2f8a8865963198b696e0ea52550
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 16:02:57 -0300

    feat: include GiveClasses screen in AppStack

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../screens/Landing';
+import GiveClasses from '../screens/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const AppStack = () => (
   <NavigationContainer>
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Landing" component={Landing} />
+      <Screen name="GiveClasses" component={GiveClasses} />
     </Navigator>
   </NavigationContainer>
 );
