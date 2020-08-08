96e6e29e9fde71acf38b2a7cc8cc264b825d4b97
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 20:12:00 -0300

    feat: add PageHeader component in Favorites screen

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 const Favorites: React.FC = () => (
-  <View style={styles.container} />
+  <View style={styles.container}>
+    <PageHeader title="Meus proffys favoritos" />
+  </View>
 );
 
 export default Favorites;
