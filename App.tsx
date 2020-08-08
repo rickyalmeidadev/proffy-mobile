cf3b512218a16f316300cb11cd9fd6dabd05efea
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-07 23:35:44 -0300

    feat: add Landing screen to App

--- App.tsx
+++ App.tsx
@@ -1,26 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import Landing from './src/screens/Landing';
 
 const App: React.FC = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Hello from React Native!!</Text>
-    <StatusBar style="light" backgroundColor="#000" />
-  </View>
+  <>
+    <StatusBar style="auto" />
+    <Landing />
+  </>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#282c34',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#69dafb',
-    fontSize: 24,
-  },
-});
 
 export default App;
