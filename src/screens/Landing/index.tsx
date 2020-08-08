453b8e736d683aef057f59dff8d5ac58c176c32f
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 00:10:51 -0300

    feat: finish Landing screen structure

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import {
+  View, Text, Image, TouchableOpacity,
+} from 'react-native';
 
 import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing = () => (
   <View style={styles.container}>
@@ -14,6 +19,25 @@ const Landing = () => (
       <Text style={styles.titleBold}>
         O que deseja fazer?
       </Text>
+    </Text>
+
+    <View style={styles.buttonsContainer}>
+      <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <Image source={studyIcon} />
+
+        <Text style={styles.buttonText}>Estudar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <Image source={giveClassesIcon} />
+
+        <Text style={styles.buttonText}>Dar aulas</Text>
+      </TouchableOpacity>
+    </View>
+
+    <Text style={styles.totalConnections}>
+      Total de 285 conexões já realizadas {' '}
+      <Image source={heartIcon} />
     </Text>
   </View>
 );
