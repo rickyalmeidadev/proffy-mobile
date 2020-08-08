54fa9a5b52c05a5a3aae320794bacc15fab1b179
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 20:03:56 -0300

    feat: create PageHeader component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
+
+import styles from './styles';
+
+const PageHeader: React.FC = () => (
+  <View style={styles.container}>
+    <View style={styles.topBar}>
+      <BorderlessButton>
+        <Image source={backIcon} resizeMode="contain" />
+      </BorderlessButton>
+
+      <Image source={logoImg} resizeMode="contain" />
+    </View>
+
+    <Text style={styles.title}>Proffys disponíveis</Text>
+  </View>
+);
+
+export default PageHeader;
