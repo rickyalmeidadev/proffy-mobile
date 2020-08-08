bbbdd0aaf8f7f5f3fa2b575d12e4746f2196f71d
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 00:10:14 -0300

    feat: load Archivo and Poppins fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
 import Landing from './src/screens/Landing';
 
-const App: React.FC = () => (
-  <>
-    <StatusBar style="auto" />
-    <Landing />
-  </>
-);
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Landing />
+    </>
+  );
+};
 
 export default App;
