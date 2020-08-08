7bbdccdab3d62499af9716a2618a8b52c95e7259
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 17:40:22 -0300

    feat: add navigation in GiveClasses screen

--- src/screens/GiveClasses/index.tsx
+++ src/screens/GiveClasses/index.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import { View, ImageBackground, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import giveClassesBgImg from '../../assets/images/give-classes-background.png';
 
 import styles from './styles';
 
-const GiveClasses: React.FC = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      source={giveClassesBgImg}
-      resizeMode="contain"
-      style={styles.content}
-    >
-      <Text style={styles.title}>Quer ser um Proffy?</Text>
-      <Text style={styles.description}>
-        Para começar, você precisa se cadastrar como professor em nossa plataforma web.
-      </Text>
-    </ImageBackground>
+const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
 
-    <RectButton style={styles.okButton}>
-      <Text style={styles.okButtonText}>Tudo bem</Text>
-    </RectButton>
-  </View>
-);
+  const handleNavigateBack = () => {
+    goBack();
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBgImg}
+        resizeMode="contain"
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor em nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+};
 
 export default GiveClasses;
