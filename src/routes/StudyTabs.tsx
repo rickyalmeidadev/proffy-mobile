021c7fe786cae8a96cc0f4a57434492fc9017c64
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 19:47:42 -0300

    feat: add styles and configs to bottom tabs

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import TeachersList from '../screens/TeachersList';
 import Favorites from '../screens/Favorites';
@@ -7,9 +9,58 @@ import Favorites from '../screens/Favorites';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const StudyTabs: React.FC = () => (
-  <Navigator>
-    <Screen name="TeachersList" component={TeachersList} />
-    <Screen name="Favorites" component={Favorites} />
+  <Navigator
+    tabBarOptions={{
+      style: {
+        elevation: 0,
+        shadowOpacity: 0,
+        height: Platform.OS === 'ios' ? 84 : 64,
+      },
+      tabStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: Platform.OS === 'ios' ? 20 : 0,
+      },
+      safeAreaInsets: {
+        bottom: 0,
+      },
+      iconStyle: {
+        flex: 0,
+        width: 20,
+        height: Platform.OS === 'ios' ? 24 : 20,
+      },
+      labelStyle: {
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 13,
+        marginLeft: 16,
+      },
+      inactiveBackgroundColor: '#fafafc',
+      activeBackgroundColor: '#ebebf5',
+      inactiveTintColor: '#c1bccc',
+      activeTintColor: '#32264d',
+    }}
+  >
+    <Screen
+      name="TeachersList"
+      component={TeachersList}
+      options={{
+        tabBarLabel: 'Proffys',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="ios-easel" color={color} size={size} />
+        ),
+      }}
+    />
+    <Screen
+      name="Favorites"
+      component={Favorites}
+      options={{
+        tabBarLabel: 'Favoritos',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="ios-heart" color={color} size={size} />
+        ),
+      }}
+    />
   </Navigator>
 );
 
