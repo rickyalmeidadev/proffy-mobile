f5cc00de706e8ea9a27a2fb85280f06822240127
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-08 22:26:23 -0300

    feat: finish TeacherItem component structure

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styles from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 const TeacherItem: React.FC = () => (
   <View style={styles.container}>
@@ -15,6 +20,31 @@ const TeacherItem: React.FC = () => (
         <Text style={styles.subject}>Matemática</Text>
       </View>
     </View>
+
+    <Text style={styles.bio}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      {'\n'}{'\n'}
+      Possimus eligendi pariatur natus ipsum soluta repellendus quae ratione, earum debitis amet!
+    </Text>
+
+    <View style={styles.footer}>
+      <Text style={styles.price}>
+        Preço/hora {'   '}
+        <Text style={styles.priceValue}>R$ 20,00</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton style={styles.favoriteButton}>
+          <Image source={heartOutlineIcon} />
+        </RectButton>
+
+        <RectButton style={styles.contactButton}>
+          <Image source={whatsappIcon} />
+          <Text style={styles.contactButtonText}>Entrar em contato</Text>
+        </RectButton>
+      </View>
+    </View>
+
   </View>
 );
 
